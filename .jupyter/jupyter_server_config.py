43990a3700df2ff6c8cba8d05084db8ece2a8925
kenalger@comcast.net <kenalger@comcast.net>
2025-05-29 12:52:17 -0700

    fix: Remove pgcontents from jupyter_server_config.py
    Comments out the sections in `.jupyter/jupyter_server_config.py`
    that were attempting to configure and use `pgcontents.PostgresContentsManager`.
    
    This was causing a `ModuleNotFoundError` for `pgcontents` after
    switching the application to use local filesystem storage for notebooks,
    as `pgcontents` is no longer an installed dependency.
    
    The Jupyter server should now default to `FileSystemContentsManager`
    and correctly use the `--notebook-dir` specified in `start_jupyter`.

--- .jupyter/jupyter_server_config.py
+++ .jupyter/jupyter_server_config.py
@@ -3,17 +3,17 @@ try:
     import json
     import traceback
     import IPython.lib
-    import pgcontents
+    # import pgcontents # DEACTIVATED - Switched to FileSystemContentsManager
 
-    c = get_config()
+    c = get_config() # type: ignore # noqa F821
 
-    # Root directory for notebook
-    c.ServerApp.root_dir='/'
+    # Root directory for notebook - This might be overridden by --notebook-dir in start_jupyter
+    # c.ServerApp.root_dir='/'
 
     ### Password protection ###
     # http://jupyter-notebook.readthedocs.io/en/latest/security.html
     if os.environ.get('JUPYTER_NOTEBOOK_PASSWORD_DISABLED') != 'DangerZone!':
-        passwd = os.environ['JUPYTER_NOTEBOOK_PASSWORD']
+        passwd = os.environ.get('JUPYTER_NOTEBOOK_PASSWORD', 'heroku') # Provide default if not set
         c.ServerApp.password = IPython.lib.passwd(passwd)
     else:
         c.ServerApp.token = ''
@@ -22,27 +22,27 @@ try:
     ### Make it so the default shell is bash & the prompt is not awful:
     c.ServerApp.terminado_settings = {'shell_command': ['/bin/bash']}
 
-    ### PostresContentsManager ###
-    database_url = os.getenv('DATABASE_URL', None)
-    if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    if database_url:
-        # Tell IPython to use PostgresContentsManager for all storage.
-        c.ServerApp.contents_manager_class = pgcontents.PostgresContentsManager
-
-        # Set the url for the database used to store files.  See
-        # http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
-        # for more info on db url formatting.
-        c.PostgresContentsManager.db_url = database_url
-
-        # PGContents associates each running notebook server with a user, allowing
-        # multiple users to connect to the same database without trampling each other's
-        # notebooks. By default, we use the result of result of getpass.getuser(), but
-        # a username can be specified manually like so:
-        c.PostgresContentsManager.user_id = 'heroku'
-
-        # Set a maximum file size, if desired.
-        #c.PostgresContentsManager.max_file_size_bytes = 1000000 # 1MB File cap
+    ### PostresContentsManager ### # DEACTIVATED SECTION
+    # database_url = os.getenv('DATABASE_URL', None)
+    # if database_url and database_url.startswith("postgres://"):
+    #     database_url = database_url.replace("postgres://", "postgresql://", 1)
+    # if database_url:
+    #     # Tell IPython to use PostgresContentsManager for all storage.
+    #     c.ServerApp.contents_manager_class = pgcontents.PostgresContentsManager
+    #
+    #     # Set the url for the database used to store files.  See
+    #     # http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
+    #     # for more info on db url formatting.
+    #     c.PostgresContentsManager.db_url = database_url
+    #
+    #     # PGContents associates each running notebook server with a user, allowing
+    #     # multiple users to connect to the same database without trampling each other's
+    #     # notebooks. By default, we use the result of result of getpass.getuser(), but
+    #     # a username can be specified manually like so:
+    #     c.PostgresContentsManager.user_id = 'heroku'
+    #
+    #     # Set a maximum file size, if desired.
+    #     #c.PostgresContentsManager.max_file_size_bytes = 1000000 # 1MB File cap
 
 except Exception:
     traceback.print_exc()
